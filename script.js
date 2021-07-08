fff52997543ed358c63c76e4d0b4b8b31e2c49b2
GabrielaTicu <78794186+GabrielaTicu@users.noreply.github.com>
2021-07-08 21:43:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function onClick(id) {
-    var n = document.getElementById(id).nodeValue;
+    var n = document.getElementById(id).text;
     n = Number(n);
     n++;
     document.getElementById(id).innerHTML = n;
